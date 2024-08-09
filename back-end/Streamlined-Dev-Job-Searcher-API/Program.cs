0c97ff293d6f412dc36bd5e7cdee04d09caf3476
Scott Cooper <18429915+PlasmaDiffusion@users.noreply.github.com>
2024-08-09 17:07:13 -0400

    Logger class

--- back-end/Streamlined-Dev-Job-Searcher-API/Program.cs
+++ back-end/Streamlined-Dev-Job-Searcher-API/Program.cs
@@ -16,34 +16,6 @@ builder.Services.AddDefaultAWSOptions(awsOptions);
 builder.Services.AddAWSService<IAmazonDynamoDB>();
 builder.Services.AddScoped<IDynamoDBContext, DynamoDBContext>();
 
-// create a logger factory
-var loggerFactory = LoggerFactory.Create(
-    builder => builder
-                // add console as logging target
-                .AddConsole()
-                // add debug output as logging target
-                .AddDebug()
-                // set minimum level to log
-                .SetMinimumLevel(LogLevel.Debug)
-);
-
-// create a logger
-var logger = loggerFactory.CreateLogger<Program>();
-
-// logging
-logger.LogTrace("Trace message");
-logger.LogDebug("Debug message");
-logger.LogInformation("Info message");
-logger.LogWarning("Warning message");
-logger.LogError("Error message");
-logger.LogCritical("Critical message");
-
-builder.Configuration.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("secrets.json");
-logger.LogInformation("*AccessKey " + builder.Configuration.GetConnectionString("AccessKey"));
-logger.LogInformation("*SecretKey " + builder.Configuration["SecretKey"]);
-logger.LogInformation("*AWS " + builder.Configuration.GetConnectionString("AWS"));
-
-
 
 var app = builder.Build();
 
---
+++ back-end/Streamlined-Dev-Job-Searcher-API/classes/Logger.cs
@@ -0,0 +1,58 @@
+public class Logger
+{
+    static ILogger<Program>? logger;
+
+    Logger()
+    {
+        // create a logger factory
+        var loggerFactory = LoggerFactory.Create(
+            builder => builder
+                        // add console as logging target
+                        .AddConsole()
+                        // add debug output as logging target
+                        .AddDebug()
+                        // set minimum level to log
+                        .SetMinimumLevel(LogLevel.Debug)
+        );
+        logger = loggerFactory.CreateLogger<Program>();
+    }
+
+    public enum LogTypes
+    {
+        LOG,
+        DEBUG,
+        INFO,
+        WARNING,
+        ERROR,
+        CRITICAL
+    }
+
+    public static void Log(string message, LogTypes type = LogTypes.INFO)
+    {
+        if (logger != null)
+        {
+            switch (type)
+            {
+                case LogTypes.LOG:
+                    logger.LogTrace(message);
+                    break;
+                case LogTypes.DEBUG:
+                    logger.LogDebug(message);
+                    break;
+                case LogTypes.INFO:
+                    logger.LogInformation(message);
+                    break;
+                case LogTypes.WARNING:
+                    logger.LogWarning(message);
+                    break;
+                case LogTypes.ERROR:
+                    logger.LogError(message);
+                    break;
+                case LogTypes.CRITICAL:
+                    logger.LogCritical(message);
+                    break;
+            }
+        }
+
+    }
+}
